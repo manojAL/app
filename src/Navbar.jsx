783b48c757a6c8548f4acb85fcd417c01338c639
manojAL <manoj35ramesh@gmail.com>
2025-01-11 13:25:05 +0530

    reactive

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,8 +1,12 @@
-import './Navbar.css'
+import './Navbar.css';
+import React,{ useState } from 'react';
 export default Navbar;
 function Navbar(){
+    const [menuOpen,setMenuOpen]=useState(false);
+   
 return(
-    <div className="navbar">
+    <div>
+        <div className="navbar">
         <div className="logo"><a href="#" className='logo-link'>Volunteer</a></div>
         
         <ul className='navbar-links'>
@@ -11,11 +15,23 @@ return(
             <li><a href="explore">Explore</a></li>
             <li><a href="profile">Profile</a></li>
         </ul>
-        <div className="toggle-btn" onclick="toggleMenu()"><i class="fa-solid fa-bars"></i></div>
+        <div className="toggle-btn" onClick={()=>{
+            setMenuOpen(!menuOpen);
+        }}><i class="fa-solid fa-bars"></i></div>
+    
+      
     </div>
+    <ul className={menuOpen?"toggle-bar":"no-toggle"}>
+            <li><a>GO</a></li>
+            <li><a>Login</a></li>
+            <li><a>Explore</a></li>
+            <li><a>Profile</a></li>
+        </ul>
+
+    </div>
+
+
+   
 )
 }
-function toggleMenu() {
-    const navbarLinks = document.querySelector('.navbar-links');
-    navbarLinks.classList.toggle('active');
-}
+
