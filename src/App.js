6e7642f4893584cbc13082b3a01c65d09eb4fa73
manojAL <manoj35ramesh@gmail.com>
2025-01-13 12:26:14 +0530

    transition and efffects

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@
 import './App.css';
 import Cards from './Cards.jsx';
 import Navbar from './Navbar.jsx';
+import Section from './Section.jsx';
 function App() {
   return (
     <div className='body'>
       <Navbar/>
       <Cards/>
+      <Section/>
     </div>
   )
 }
---
+++ src/Section.jsx
@@ -0,0 +1,20 @@
+import './Section.css';
+function Section(){
+            
+    return(
+     <div className="section">
+        <div>
+         <div className='manoj'>Volunteer for the cause</div>
+         <h1>Start giving back to the community today!</h1>
+         <img className="circle"src="/images/circle.svg"></img>
+         <img className="slash"src="/images/slash.svg"></img>
+         <p>Together, we shape a narrative of shared responsibility, building bridges and transforming challenges into opportunities.
+          </p>
+        </div>
+        <div>
+         <img src="/images/sectionimg.svg"></img>
+        </div>
+     </div>
+  )
+}
+export default Section 
